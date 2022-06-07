05604a6f3ec3d651d7337808b15f32f4291a48ac
Nizzou <nizzou.only@gmail.com>
2022-06-07 20:15:58 +0200

    Add SetPresence

--- events/client/ready.js
+++ events/client/ready.js
@@ -4,7 +4,11 @@ module.exports  = {
     name: 'ready',
     once: true,
     async execute(client) {
+
         Logger.client("- Connecté");
+        
+        client.user.setPresence({ activities: [{ name: '!help', type: 'WATCHING'}], status: 'dnd'});
+
 
         // const devGuild = await client.guilds.cache.get('971132553530601543');
         // devGuild.commands.set(client.commands.map(cmd => cmd));
